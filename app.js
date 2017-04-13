5d7544a36613605b3a38ccf4a49303b0b934d95d
Felipe Arcos <efarcos@unicauca.edu.co>
2017-04-12 21:42:39 -0500

    BacKend en sus inicios

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb').MongoClient;
+
+var novedades = require('./routes/novedades');
+
+
+var app = express();
+
+let dbConnection;
+mongodb.connect("mongodb://pipearcos221:pipearcos221@ds157380.mlab.com:57380/oferton", (err, db) => {
+
+  if (err) {
+    console.log("Error:" + err);
+  } else {
+    console.log("Mongo Conectado");
+    dbConnection = db;
+  }
+
+});
+
+app.use((req, res, next) => {
+
+  req.db = dbConnection;
+  next();
+
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('./novedades', novedades);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/novedades.js
@@ -0,0 +1,76 @@
+var express = require("express");
+var router = express.Router();
+var ObjectID = require("mongodb").ObjectID;
+
+router.use((req, res, next) => {
+    req.collection = req.db.collection("novedades");
+    next();
+});
+
+router.get("/", (req, res, next) => {
+    req.collection.find().toArray().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.send([]);
+    });
+});
+
+router.get("/:id", (req, res, next)=>{
+    let id = new ObjectID(req.params.id);
+    req.collection.findOne({_id:id}).then(doc=>{
+        if(doc){
+            res.send(doc);
+        }else{
+            res.status(404).send({msg:"Promocion no encontrada"});
+        }
+    }).catch(err => {
+
+    });
+});
+
+// router.get("/:almacen", (req, res, next)=>{
+//     let almacen = new ObjectID(req.params.id);
+//     req.collection.find({_almacen:almacen}).toArray().then(doc=>{
+//         if(doc){
+//             res.send(doc);
+//         }else{
+//             res.status(404).send({msg:"Almacen no encontrado"});
+//         }
+//     }).catch(err => {
+
+//     });
+// });
+
+router.post("/", (req, res, next) => {
+    let novedad = req.body;
+    req.collection.insert(novedad).then(result => {
+        res.send({ success: true });
+    }).catch(err => {
+        res.send({ success: false });
+    });
+});
+
+router.put("/:id", (req, res, next) => {
+    let id = new ObjectID(req.params.id);
+    let novedad = req.body;
+    req.collection.updateOne({_id:id}, {$set:novedad}).then(result=>{
+        res.send({success:true});
+    }).catch(err=>{
+        res.send({success:false})
+    });
+
+   
+});
+
+router.delete("/:id", (req, res, next) => {
+    let id = new ObjectID(req.params.id);
+    req.collection.deleteOne({_id:id}).then(result=>{
+        res.send({success:true});
+    })
+    .catch(err=>{
+        res.send({success:false});
+    
+    }); 
+});
+
+module.exports = router;
